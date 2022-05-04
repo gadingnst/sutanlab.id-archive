36482ac49de10e9016e0a279b7d1a100569138de
Sutan Gading Nasution <contact@sutanlab.id>
2022-05-05 03:46:36 +0700

    feat: add nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
-import type { FunctionComponent } from 'react';
 import type { AppProps } from 'next/app';
+import { Fragment, FunctionComponent } from 'react';
+import NProgress from 'nextjs-progressbar';
+import useAppTheme from 'hooks/stores/useAppTheme';
 import 'styles/globals.css';
 
 const App: FunctionComponent<AppProps> = (props) => {
   const { Component, pageProps } = props;
+  const [theme] = useAppTheme();
+  const nprogressColor = theme.current === 'light' ? '#B89BFF' : '#5E72E4';
   return (
-    <Component {...pageProps} />
+    <Fragment>
+      <NProgress color={nprogressColor} />
+      <Component {...pageProps} />
+    </Fragment>
   );
 };
 
